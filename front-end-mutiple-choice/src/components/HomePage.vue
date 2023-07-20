939e4222bc5b79b4102ae3a1b50f413ade7530ca
duyminh0908 <minhsatthu0908@gmail.com>
2023-07-20 14:29:51 +0700

    add lib

--- front-end-mutiple-choice/src/components/HomePage.vue
+++ front-end-mutiple-choice/src/components/HomePage.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
 import { useCount } from "../store/countStore";
-
+import Logincomponent from "../components/Login.vue";
 const counterStore = useCount();
 </script>
 <template>
   <h1 class="text-blue-500">Hello guy, this is page home</h1>
   <h1>{{ counterStore.getCount }}</h1>
   <button @click="counterStore.setCount">Click me</button>
+  <Logincomponent />
 </template>
