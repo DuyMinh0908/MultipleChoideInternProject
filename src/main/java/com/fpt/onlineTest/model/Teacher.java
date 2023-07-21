567d959199cda20e414fde117b4c1b02700ed42a
HoViet0107 <hoquocviet.na.da@gmail.com>
2023-07-21 15:44:38 +0700

    Model User + Teacher + Admin

--- src/main/java/com/fpt/onlineTest/model/Teacher.java
+++ src/main/java/com/fpt/onlineTest/model/Teacher.java
@@ -7,49 +7,44 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
-
 @Entity
-@Table(name = "Users")
+@Table(name = "Teachers")
 @AllArgsConstructor
 @NoArgsConstructor
-@Setter
 @Getter
-@Data
-public class User implements Serializable {
+@Setter
+@Builder
+public class Teacher implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer userId;
+    private Integer teacherId;
     @Column(columnDefinition = "varchar(20) not null")
     private String username;
     @Column(columnDefinition = "varchar(16)not null")
     private String userPass;
     @Column(columnDefinition = "nvarchar(50) not null")
     private String fullName;
+
     private String email;
     @Column(columnDefinition = "varchar(10)not null")
     private String phone;
     @Column(columnDefinition = "nvarchar(100) not null")
     private String address;
     @Column(columnDefinition = "varchar(255)not null")
-    private String imageUser;
-//    @Column(columnDefinition = "varchar(10)not null")
-//    private String role;
+    private String imageTeacher;
+//    private Integer roleId;
 
     @ManyToOne
     @JoinColumn(name = "roleId")
     private Role role;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "teacher")
     List<Course> courses;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "user")
-    List<Answer> answers;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "user")
-    List<ResultExam> resultExams;
-
-
+//
+//    @JsonIgnore
+//    @OneToMany(mappedBy = "user")
+//    List<ResultExam> resultExams;
 }
