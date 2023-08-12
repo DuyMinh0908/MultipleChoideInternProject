b7aa0f75f75581530713aa84b2e61982885ec96d
duong201 <truongtheduong88@gmail.com>
2023-08-12 15:01:28 +0700

    config login

--- src/main/java/com/fpt/onlineTest/dto/UserConnectDto.java
+++ src/main/java/com/fpt/onlineTest/dto/UserConnectDto.java
@@ -11,5 +11,12 @@ import lombok.Setter;
 @Setter
 public class UserConnectDto {
     private Integer id;
+    private String address;
+    private String email;
+    private String fullName;
+    private String image;
+    private String phone;
+    private String username;
+    private String password;
     private Integer role;
 }
--- src/main/java/com/fpt/onlineTest/restController/UserConnectController.java
+++ src/main/java/com/fpt/onlineTest/restController/UserConnectController.java
@@ -1,7 +1,9 @@
 package com.fpt.onlineTest.restController;
 
 import com.fpt.onlineTest.dto.UserConnectDto;
+import com.fpt.onlineTest.model.Admin;
 import com.fpt.onlineTest.model.ResponseObject;
+import com.fpt.onlineTest.model.Teacher;
 import com.fpt.onlineTest.model.User;
 import com.fpt.onlineTest.service.UserConnectService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,8 +37,45 @@ public class UserConnectController {
                                                 @RequestParam("password") String password) {
         try{
             User user = userConnectService.login(username.trim(), password.trim());
+            Teacher teacher = userConnectService.loginTeacher(username.trim(), password.trim());
+            Admin admin = userConnectService.loginAdmin(username.trim(), password.trim());
+            UserConnectDto userConnectDto = new UserConnectDto();
 
-            return new ResponseEntity<>(new ResponseObject("success", "Login successfully!!", user), HttpStatus.OK);
+            if(user == null) {
+                if(teacher == null) {
+                    userConnectDto.setId(admin.getAdminId());
+                    userConnectDto.setAddress(" ");
+                    userConnectDto.setEmail(" ");
+                    userConnectDto.setFullName(" ");
+                    userConnectDto.setImage(" ");
+                    userConnectDto.setPhone(" ");
+                    userConnectDto.setUsername(admin.getUsername());
+                    userConnectDto.setPassword(admin.getUserPass());
+                    userConnectDto.setRole(admin.getRole().getRoleId());
+                }else{
+                    userConnectDto.setId(teacher.getId());
+                    userConnectDto.setAddress(teacher.getAddress());
+                    userConnectDto.setEmail(teacher.getEmail());
+                    userConnectDto.setFullName(teacher.getFullName());
+                    userConnectDto.setImage(teacher.getImageTeacher());
+                    userConnectDto.setPhone(teacher.getPhone());
+                    userConnectDto.setUsername(teacher.getUsername());
+                    userConnectDto.setPassword(teacher.getUserPass());
+                    userConnectDto.setRole(teacher.getRole().getRoleId());
+                }
+            }else{
+                userConnectDto.setId(user.getUserId());
+                userConnectDto.setAddress(user.getAddress());
+                userConnectDto.setEmail(user.getEmail());
+                userConnectDto.setFullName(user.getFullName());
+                userConnectDto.setImage(user.getImageUser());
+                userConnectDto.setPhone(user.getPhone());
+                userConnectDto.setUsername(user.getUsername());
+                userConnectDto.setPassword(user.getUserPass());
+                userConnectDto.setRole(user.getRole().getRoleId());
+            }
+
+            return new ResponseEntity<>(new ResponseObject("success", "Login successfully!!", userConnectDto), HttpStatus.OK);
         }catch (Exception e) {
             return new ResponseEntity<>(new ResponseObject("failed", "Username or password is incorrect!!", ""), HttpStatus.NOT_FOUND);
         }
